908a9537ba77d6ca7eddb9549743c94362dedcb1
dhanushkomari <dhanushkomari@gmail.com>
2020-10-10 09:52:46 +0530

    made some changes

--- GRIDLEX_TASK.py
+++ GRIDLEX_TASK.py
@@ -433,7 +433,7 @@ def PRINTING_PATTERN():
             u= [[" " for i in range(6)] for j in range(6)]
             for row in range(6):
                 for col in range(6):
-                    if (col==0 and row!=5) or (col==3 and row!=5) or (row==5 and (col>0 and col<3)):
+                    if (col==1 and row!=5) or (col==4 and row!=5) or (row==5 and (col==2 or col==3)):
                         u[row][col]="#"
             List.append(u)
 
@@ -460,7 +460,7 @@ def PRINTING_PATTERN():
             x= [[" " for i in range(6)] for j in range(6)]
             for row in range(6):
                 for col in range(6):
-                    if (col==2 and (row>0 and row<5)) or (col==3 and (row>0 and row<5)) or (row==0 and (col>3 or col<2)) or (row==5 and (col>3 or col<2)):
+                    if row==col or (row==5 and col==0) or (row==4 and col==1) or (row==3 and col==2) or (row==2 and col==3) or (row==1 and col==4) or (row==0 and col==5):
                         x[row][col]="#"
             List.append(x)
 
